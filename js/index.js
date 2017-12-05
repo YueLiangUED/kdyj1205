f11a2641bef6f9afbb3520b71dd5dea7eed7772d
ChengYien <cye_orange@163.com>
2017-12-05 16:48:47 +0800

    a

--- js/index.js
+++ js/index.js
@@ -1,28 +1,5 @@
 $(function () {
 
-    // 日期选择
-    (function () {
-        var currYear = (new Date()).getFullYear();
-        var opt={};
-        opt.date = {preset : 'date'};
-        opt.datetime = {preset : 'datetime'};
-        opt.time = {preset : 'time'};
-        opt.default = {
-            theme: 'android-ics light', //皮肤样式
-            display: 'modal', //显示方式
-            mode: 'scroller', //日期选择模式
-            dateFormat: 'yyyy-mm-dd',
-            lang: 'zh',
-            showNow: true,
-            nowText: "今天",
-            startYear: currYear - 0, //开始年份
-            endYear: currYear + 100, //结束年份
-            minDate: new Date()
-        };
-
-        $("#selectDate").mobiscroll($.extend(opt['date'], opt['default']));
-    })();
-
     function modalShow(ele) {
         $('.mask').fadeIn();
         $(ele).fadeIn();
@@ -35,9 +12,11 @@ $(function () {
 
     $('.modal-enter').on('touchend', function () {
         modalHide(this);
+        window.location.href = 'index_02.html'
     });
 
     $('.yj-form-sub').on('touchend', function () {
         modalShow('.modal');
     });
+
 });
