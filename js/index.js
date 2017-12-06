0442d0252f943fc591e3f0ba518f0e615a29dcb7
ChengYien <cye_orange@163.com>
2017-12-06 10:38:36 +0800

    a

--- js/index.js
+++ js/index.js
@@ -1,22 +1,4 @@
 $(function () {
 
-    function modalShow(ele) {
-        $('.mask').fadeIn();
-        $(ele).fadeIn();
-    }
-    function modalHide(ele) {
-        var $modal = $(ele).parents('.modal');
-        $('.mask').fadeOut();
-        $modal.fadeOut();
-    }
-
-    $('.modal-enter').on('touchend', function () {
-        modalHide(this);
-        window.location.href = 'index_02.html'
-    });
-
-    $('.yj-form-sub').on('touchend', function () {
-        modalShow('.modal');
-    });
 
 });
